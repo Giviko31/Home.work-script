09f28bacbb9034557ef73d759720f05f26abecd3
Giviko31 <givigivishvili209@gmail.com>
2023-01-15 19:14:59 +0400

    Wrote javascript maths

---
+++ script.js
@@ -0,0 +1,79 @@
+// math
+
+let x = 5
+let y = 10
+
+let math = x + y 
+console.log(math);
+
+// _________________________
+
+let apple = 100
+let banana = 15
+
+let fruit = apple - banana
+console.log(fruit);
+
+// ____________________________
+
+
+let o = 10
+let p = 10
+
+let red = o * p
+console.log(red);
+
+// _______________________________
+
+
+let k = 100
+let b = 5 
+
+let black = k / b
+console.log(black);
+
+// ____________________
+
+// string concatenation
+
+// let user = 'Givi'
+// let surName = 'Givishvili'
+// let age = 26;
+
+// console.log (user + " " + surName)
+
+// ___________________________________
+
+
+let user = 'Givi'
+let surName = 'Givishvili'
+console.log(`${user} ${surName}`);
+
+
+// __________________________________
+
+// დავალება [ 4 ]
+
+
+let length = 7
+let width = 5 
+
+let rectangle = 7 * 5
+console.log(rectangle);
+
+//______________________________
+
+// დავალება [ 5 ]
+
+const PI = Math.PI;
+let radius = 7;
+let circle;
+
+radius = Number(radius);
+
+circle = 2 * PI * radius;
+
+console.log(circle);
+
+
+// __________________________
